c183fc45eaef145232514c9021378f9b6a5d3977
macmribo <98993577+macmribo@users.noreply.github.com>
2022-07-01 09:00:37 -0600

    I added an array

--- first_code.py
+++ first_code.py
@@ -4,12 +4,16 @@ import sys
 
 import numpy as np
 import requests
+from numpy.linalg import inv
 
 tot_output = np.array([5000, 10000])
 flow_tbl = np.array([[350, 400], [275, 100]])  # Flows table/intermediate table
 # Technical coefficient matrix
-mx_A = flow_tbl.dot(np.linalg.inv(tot_output * np.identity(2)))
+mx_A = flow_tbl.dot(inv(tot_output * np.identity(2)))
 # Technology matrix
-mx_B = np.linalg.inv(np.identity(2) - mx_A)
+mx_B = inv(np.identity(2) - mx_A)
 print(mx_A)
 print(mx_B)
+
+# Adding another thing
+thing = np.array([1, 2])
