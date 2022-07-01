529c1c987061c5d16b56b6d8dcfcda5ea990e745
macmribo <98993577+macmribo@users.noreply.github.com>
2022-06-30 20:48:23 -0600

    I made a technology matrix

--- first_code.py
+++ first_code.py
@@ -5,5 +5,11 @@ import sys
 import numpy as np
 import requests
 
-name = input('Your name?')
-print("Hello, ", name)
+tot_output = np.array([5000, 10000])
+flow_tbl = np.array([[350, 400], [275, 100]])  # Flows table/intermediate table
+# Technical coefficient matrix
+mx_A = flow_tbl.dot(np.linalg.inv(tot_output * np.identity(2)))
+# Technology matrix
+mx_B = np.linalg.inv(np.identity(2) - mx_A)
+print(mx_A)
+print(mx_B)
